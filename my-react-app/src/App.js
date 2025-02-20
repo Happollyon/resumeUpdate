5792dca78c3303e11299673adcb4083090d284da
Happollyon <36013973+Happollyon@users.noreply.github.com>
2025-02-20 14:40:39 +0000

    first page

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import React from "react";
-import ReactMarkdown from "react-markdown";
+import MarkToCv from './pages/MarkToCv';
 
 function App() {
   return (
     <div className="App">
-     
+      
+      <MarkToCv />
       
     </div>
   );
