aa0ee7077acdb16a68ca752df674f717e57acdbf
Happollyon <36013973+Happollyon@users.noreply.github.com>
2025-02-20 12:42:13 +0000

    new project

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React from "react";
+import ReactMarkdown from "react-markdown";
+
+function App() {
+  return (
+    <div className="App">
+     
+      
+    </div>
+  );
+}
+
+export default App;
